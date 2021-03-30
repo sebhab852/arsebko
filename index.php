3ff046898d4e4c3ddb0d65eb8fe17a4af0fe1b29
Marko Zepic <zepic@live.at>
2021-03-30 12:42:39 +0200

    controller.js included

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        
         <link rel="stylesheet" type="text/css" href="res/style/style.css">
+        <script src="client/controller.js"></script>
         
         <title>Arsebko - Business ideas and more</title>
     </head>
