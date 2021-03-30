a40b799189b008574b5dadbedab364e5d9792e8a
Sebastian Haberler <you@example.comexit>
2021-03-30 17:30:10 +0200

    login und registration template

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         
-        <link rel="stylesheet" type="text/css" href="res/style/style.css">
+        <link rel="stylesheet" type="text/css" href="res/css/style.css">
         <script src="client/controller.js"></script>
         
         <title>Arsebko - Business ideas and more</title>
@@ -21,6 +21,7 @@
 
     
     <body>
+        <?php include "inc/registration_business.html"; ?>
         
 
 
