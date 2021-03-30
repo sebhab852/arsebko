477e161524d99f27c28d33b5980d8e17dfa8ca47
Marko Zepic <zepic@live.at>
2021-03-30 12:40:12 +0200

    okok

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
+
 <html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="res/style/style.css">
+        
+        <title>Arsebko - Business ideas and more</title>
+    </head>
 
-    <link rel="stylesheet" type="text/css" href="res/style/style.css">
     
-    <title>Arsebko - Business ideas and more</title>
-</head>
-
-<body>
-    
+    <body>
+        
 
 
 
@@ -21,5 +22,6 @@
 
 
 
-</body>
+    </body>
+
 </html>
